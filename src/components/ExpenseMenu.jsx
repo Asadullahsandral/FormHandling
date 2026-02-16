28456f4091079c005dbafca83e00a18334468d53
AsadullahSandral <asadkhushab@gmail.com>
2026-02-16 12:55:00 +0500

    Updated

--- src/components/ExpenseMenu.jsx
+++ src/components/ExpenseMenu.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteExpense, editExpenseStart } from "../Redux/store/expenseSlice";
 
-function ExpenseMenu() {
+function ExpenseMenu({ contextMenu, setContextMenu }) {
+  const { expenseId, title, category, amount } = contextMenu;
+
+  const dispatch = useDispatch();
+
+  if (!contextMenu.left) {
+    return null;
+  }
   return (
-    <div className="context-menu">
-      <div>Edit</div>
-      <div>Delete</div>
+    <div
+      className="context-menu"
+      style={{
+        left: contextMenu.left,
+        top: contextMenu.top,
+      }}
+    >
+      <div
+        onClick={() => {
+          console.log("Edit");
+          dispatch(
+            editExpenseStart({ id: expenseId, title, category, amount }),
+          );
+          setContextMenu({});
+        }}
+      >
+        Edit
+      </div>
+      <div
+        onClick={() => {
+          dispatch(deleteExpense(expenseId));
+          setContextMenu({});
+        }}
+      >
+        Delete
+      </div>
     </div>
   );
 }
--- src/components/Expensetable.jsx
+++ src/components/Expensetable.jsx
@@ -3,8 +3,12 @@ import { deleteExpense, editExpenseStart } from "../Redux/store/expenseSlice";
 import { MdDelete } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
 import { useFilter } from "../Hooks/useFilter";
+import ExpenseMenu from "./ExpenseMenu";
+import { useState } from "react";
 
 function Expensetable() {
+  const [contextMenu, setContextMenu] = useState({});
+
   const expenses = useSelector((state) => state.expense.expenses);
   const dispatch = useDispatch();
 
@@ -26,91 +30,107 @@ function Expensetable() {
     0,
   );
   return (
-    <div>
-      <table className="expense-table">
-        <thead>
-          <tr>
-            <th style={{ width: "700px" }}>Title</th>
-            <th style={{ width: "500px" }}>
-              <select
-                name="category"
-                id="category"
-                onChange={(e) => {
-                  setQuery(e.target.value);
-                }}
-              >
-                <option value="">All</option>
-                <option value="Grocery">Grocery</option>
-                <option value="Clothes">Clothes</option>
-                <option value="Bills">Bills</option>
-                <option value="Education">Education</option>
-                <option value="Medicine">Medicine</option>
-              </select>
-            </th>
-            <th className="amount-column">
-              <div>
-                <span>Amount</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="10"
-                  viewBox="0 0 384 512"
-                  className="arrow up-arrow"
-                >
-                  <title>Ascending</title>
-                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="10"
-                  viewBox="0 0 384 512"
-                  className="arrow down-arrow"
-                >
-                  <title>Descending</title>
-                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                </svg>
-              </div>
-            </th>
-            <th>Remove</th>
-            <th>Update</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((expense) => {
-            return (
-              <tr key={expense.id}>
-                <td>{expense.title}</td>
-                <td>{expense.category}</td>
-                <td>{expense.amount}</td>
-                <td
-                  style={{
-                    cursor: "pointer",
-                    color: "red",
-                    textAlign: "center",
+    <>
+      <ExpenseMenu contextMenu={contextMenu} setContextMenu={setContextMenu} />
+      <div>
+        <table className="expense-table" onClick={() => setContextMenu({})}>
+          <thead>
+            <tr>
+              <th style={{ width: "700px" }}>Title</th>
+              <th style={{ width: "500px" }}>
+                <select
+                  name="category"
+                  id="category"
+                  onChange={(e) => {
+                    setQuery(e.target.value);
                   }}
-                  onClick={() => dispatch(deleteExpense(expense.id))}
                 >
-                  <MdDelete />
-                </td>
-                <td
-                  style={{
-                    cursor: "pointer",
-                    color: "blue",
-                    textAlign: "center",
+                  <option value="">All</option>
+                  <option value="Grocery">Grocery</option>
+                  <option value="Clothes">Clothes</option>
+                  <option value="Bills">Bills</option>
+                  <option value="Education">Education</option>
+                  <option value="Medicine">Medicine</option>
+                </select>
+              </th>
+              <th className="amount-column">
+                <div>
+                  <span>Amount</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    viewBox="0 0 384 512"
+                    className="arrow up-arrow"
+                  >
+                    <title>Ascending</title>
+                    <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    viewBox="0 0 384 512"
+                    className="arrow down-arrow"
+                  >
+                    <title>Descending</title>
+                    <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                  </svg>
+                </div>
+              </th>
+              <th>Remove</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((expense) => {
+              return (
+                <tr
+                  key={expense.id}
+                  onContextMenu={(e) => {
+                    e.preventDefault();
+                    setContextMenu({
+                      left: e.clientX,
+                      top: e.clientY,
+                      expenseId: expense.id,
+                      title: expense.title,
+                      category: expense.category,
+                      amount: expense.amount,
+                    });
                   }}
-                  onClick={() => dispatch(editExpenseStart(expense))}
                 >
-                  <CiEdit />
-                </td>
-              </tr>
-            );
-          })}
-          <tr>
-            <th colSpan="2">Total</th>
-            <th colSpan="3">Rs.{total}</th>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+                  <td>{expense.title}</td>
+                  <td>{expense.category}</td>
+                  <td>{expense.amount}</td>
+                  <td
+                    style={{
+                      cursor: "pointer",
+                      color: "red",
+                      textAlign: "center",
+                    }}
+                    onClick={() => dispatch(deleteExpense(expense.id))}
+                  >
+                    <MdDelete />
+                  </td>
+                  <td
+                    style={{
+                      cursor: "pointer",
+                      color: "blue",
+                      textAlign: "center",
+                    }}
+                    onClick={() => dispatch(editExpenseStart(expense))}
+                  >
+                    <CiEdit />
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              <th colSpan="2">Total</th>
+              <th colSpan="3">Rs.{total}</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
